200714dd8fb81856e0f9ccb19470353fdc087a8e
DILANMW <dilanweerasinghe@icloud.com>
2023-11-18 12:14:47 +0530

    adding back end base url ✌

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import router from "./routes/route.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("https://tasks-list-green.vercel.app/", router);
+app.use("/", router);
 
 const PORT = 8000;
 
